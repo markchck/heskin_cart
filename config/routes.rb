29b9b70865adee219e3bc72f09ed02720808a5b9
wonno <jake89577@gmail.com>
2020-06-26 11:08:27 +0900

    2.1. routes 수정하기

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root "home#index"
+  
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get "packs/index"
-  get "packs/show/:id" => "packs#show"
+  resources :packs, only: [:index, :show]
 
-  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
